5b8e23b34d3862de187a60efef689667bc17907b
hayateMizuno <22t0215z@student.gs.chiba-u.jp>
2025-09-01 22:06:22 +0900

    スタッツ記録（エラーあり）

---
+++ sepaktakraw_app/Components/EnhancedStats.swift
@@ -0,0 +1,18 @@
+//
+//  EnhancedStats.swift
+//  sepaktakraw_app
+//
+//  Created by 折田研究室 on 2025/09/01.
+//
+
+import SwiftUI
+
+struct EnhancedStats: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EnhancedStats()
+}
---
+++ sepaktakraw_app/Views/StatsView.swift
@@ -0,0 +1,18 @@
+//
+//  StatsView.swift
+//  sepaktakraw_app
+//
+//  Created by 折田研究室 on 2025/09/01.
+//
+
+import SwiftUI
+
+struct StatsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatsView()
+}
